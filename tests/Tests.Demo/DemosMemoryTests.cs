feca635364fae645b74c9b66c0420c1f7d5871f3
Aleksa Ristic <aristiccitsira@gmail.com>
2025-10-27 20:31:11 -0400

    memory tests optimization

--- tests/Tests.Demo/DemosMemoryTests.cs
+++ tests/Tests.Demo/DemosMemoryTests.cs
@@ -5,6 +5,12 @@ namespace Tests.Demo;
 
 public class DemosMemoryTests
 {
+    private static readonly int DefaultMemoryLimitMb = 150;
+    private static readonly Dictionary<string, int> MemoryLimitMb = new()
+    {
+        { "Demos.Button.dll", 200 }
+    };
+
     public static IEnumerable<object[]> MainFormTypes =>
         Constants.MainForms.Select(type => new object[] { type });
 
@@ -28,9 +34,11 @@ public class DemosMemoryTests
             Thread.Sleep(Utils.Constants.InitialApplicationStartupTimeout);
             Assert.False(process!.HasExited, $"Process for {demoDllPath} exited prematurely");
             var processMemory = process.WorkingSet64;
-            double workingSetMb = processMemory / 1024.0 / 1024.0;
+            var workingSetMb = processMemory / 1024.0 / 1024.0;
+            var dllName = System.IO.Path.GetFileName(demoDllPath);
+            var limitMb = MemoryLimitMb.GetValueOrDefault(dllName, DefaultMemoryLimitMb);
             Assert.True(
-                workingSetMb < 100,
+                workingSetMb < limitMb,
                 $"Process for {demoDllPath} is using too much memory: {workingSetMb:F2} MB"
             );
         }
