bd89c06fad8f01a67e998625899bd9fb9dc19aa3
Aleksa Ristic <aristiccitsira@gmail.com>
2025-10-29 11:24:03 -0400

    Pack and publish

--- automation/pack.sh
+++ automation/pack.sh
@@ -1,3 +1,4 @@
+find .. -name "*.nupkg" -type f -delete
 for dir in ../src/CrossSharp*/; do
   if [ -f "$dir"/*.csproj ]; then
     dotnet pack "$dir"/*.csproj -c Release
--- automation/publish.sh
+++ automation/publish.sh
@@ -3,4 +3,4 @@ if \[ -z "$NUGET_KEY" \]; then
   exit 1
 fi
 
-dotnet nuget push src/LSCore.ApiClient/LSCore.ApiClient.Rest/bin/Release/*.nupkg --api-key "$NUGET_KEY" --source https://api.nuget.org/v3/index.json --skip-duplicate
+dotnet nuget push ../bin/*.nupkg --api-key "$NUGET_KEY" --source https://api.nuget.org/v3/index.json --skip-duplicate
