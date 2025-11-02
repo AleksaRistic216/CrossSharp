a8cc5f6ec3bb3deecbe78c200ea6f876013e5e29
Aleksa Ristic <aristiccitsira@gmail.com>
2025-11-01 21:48:25 -0400

    Input demos update

--- demos/Demos.Input/MainForm.cs
+++ demos/Demos.Input/MainForm.cs
@@ -22,5 +22,31 @@ public class MainForm : Form
         multiLineInput.Height = 100;
         multiLineInput.MultiLine = true;
         flowLayout.Add(multiLineInput);
+
+        var singleLineInputWithRoundedCorners = new CrossSharp.Ui.Input();
+        singleLineInputWithRoundedCorners.Width = 200;
+        singleLineInputWithRoundedCorners.Height = 30;
+        singleLineInputWithRoundedCorners.CornerRadius = 10;
+        flowLayout.Add(singleLineInputWithRoundedCorners);
+
+        var multiLineInputWithRoundedCorners = new CrossSharp.Ui.Input();
+        multiLineInputWithRoundedCorners.Width = 200;
+        multiLineInputWithRoundedCorners.Height = 100;
+        multiLineInputWithRoundedCorners.MultiLine = true;
+        multiLineInputWithRoundedCorners.CornerRadius = 10;
+        flowLayout.Add(multiLineInputWithRoundedCorners);
+
+        var singleLineInputWithPlaceholder = new CrossSharp.Ui.Input();
+        singleLineInputWithPlaceholder.Width = 200;
+        singleLineInputWithPlaceholder.Height = 30;
+        singleLineInputWithPlaceholder.Placeholder = "Enter text here...";
+        flowLayout.Add(singleLineInputWithPlaceholder);
+
+        var multiLineInputWithPlaceholder = new CrossSharp.Ui.Input();
+        multiLineInputWithPlaceholder.Width = 200;
+        multiLineInputWithPlaceholder.Height = 100;
+        multiLineInputWithPlaceholder.MultiLine = true;
+        multiLineInputWithPlaceholder.Placeholder = "Enter text here...";
+        flowLayout.Add(multiLineInputWithPlaceholder);
     }
 }
