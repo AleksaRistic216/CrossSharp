56e0f0f1ff6411fadbb0a4698ae9ee46ca3e43d3
Aleksa Ristic <aristiccitsira@gmail.com>
2025-11-03 15:11:01 -0500

    Text edit demo stage

--- demos/Demos.TextEditor/MainForm.cs
+++ demos/Demos.TextEditor/MainForm.cs
@@ -3,30 +3,40 @@ using CrossSharp.Ui;
 using CrossSharp.Utils;
 using CrossSharp.Utils.DI;
 using CrossSharp.Utils.Enums;
+using CrossSharp.Utils.EventArgs;
 using CrossSharp.Utils.Interfaces;
 
 namespace Demos.TextEditor;
 
 public class MainForm : Form
 {
+    readonly TabbedLayout _tabbedLayout = new();
     ITheme Theme => Services.GetSingleton<ITheme>();
+    string _selectedFilePath = string.Empty;
 
     public MainForm()
     {
         InitializeMenuBar();
+        InitializeTabbedLayout();
+    }
 
-        var tabbedLayout = new TabbedLayout();
-        tabbedLayout.Dock = DockStyle.Fill;
-        tabbedLayout.DockIndex = 2;
-        Controls.Add(tabbedLayout);
-        tabbedLayout.AddTab("First Tab", typeof(TextEditTab));
-        tabbedLayout.AddTabButton(
+    void InitializeTabbedLayout()
+    {
+        _tabbedLayout.Dock = DockStyle.Fill;
+        _tabbedLayout.DockIndex = 2;
+        Controls.Add(_tabbedLayout);
+        _tabbedLayout.AddTabButton(
             "+",
             () =>
             {
                 Notifications.Show("New Tab Clicked", "You clicked the new tab button.");
             }
         );
+    }
+
+    void AddTextEditTab()
+    {
+        _tabbedLayout.AddTab("New Tab", typeof(TextEditTab));
     }
 
     void InitializeMenuBar()
@@ -50,6 +60,27 @@ public class MainForm : Form
         button2.Text = "Open File";
         button2.AutoSize = true;
         button2.MinHeight = menuBarHeight;
+        button2.Click += OpenFileButton_Click;
         menuBar.Add(button2);
+    }
+
+    void OpenFileButton_Click(object? sender, EventArgs e)
+    {
+        var filesPicker = new FilesPicker();
+        filesPicker.FilesSelected += OpenFileSelected;
+        filesPicker.Show();
+    }
+
+    void OpenFileSelected(object? sender, FilesSelectedEventArgs e)
+    {
+        if (e.SelectedFiles.Length > 0)
+        {
+            var filePath = e.SelectedFiles[0];
+            Notifications.Show("File Selected", $"You selected the file: {filePath}");
+        }
+        else
+        {
+            Notifications.Show("No File Selected", "You did not select any file.");
+        }
     }
 }
