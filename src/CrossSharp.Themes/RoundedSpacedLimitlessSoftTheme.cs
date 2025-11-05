5c3450b97f7e5ededf1c4bb15e547638cd4593a7
Aleksa Ristic <aristiccitsira@gmail.com>
2025-11-05 14:20:53 -0500

    Returned base configuration form style to native

--- src/CrossSharp.Themes/RoundedSpacedLimitlessSoftTheme.cs
+++ src/CrossSharp.Themes/RoundedSpacedLimitlessSoftTheme.cs
@@ -6,14 +6,14 @@ namespace CrossSharp.Themes;
 
 public class RoundedSpacedLimitlessSoftTheme : ITheme
 {
-    public int AntiAliasingLevel { get; set; } = Constants.ANTI_ALIASING_LEVEL;
-    public RenderStyle Style { get; set; } = RenderStyle.Contained;
-    public int DefaultFontSize { get; set; } = Constants.DEFAULT_FONT_SIZE;
-    public FontFamily DefaultFontFamily { get; set; } = Constants.DEFAULT_FONT_FAMILY;
-    public ColorRgba BackgroundColor { get; set; } = ColorRgba.LimitlessBackground;
-    public ColorRgba SecondaryBackgroundColor { get; set; } = ColorRgba.LimitlessPrimary;
-    public ColorRgba ButtonBackgroundColor { get; set; } = ColorRgba.LimitlessAccent;
-    public ColorRgba InputBackgroundColor { get; set; } = ColorRgba.LimitlessInput;
-    public int DefaultCornerRadius { get; set; } = Constants.DEFAULT_ROUNDED_CORNER_RADIUS;
-    public int DefaultLayoutItemSpacing { get; set; } = Constants.DEFAULT_ROUNDED_LAYOUT_ITEM_SPACING;
+    public virtual int AntiAliasingLevel { get; set; } = Constants.ANTI_ALIASING_LEVEL;
+    public virtual RenderStyle Style { get; set; } = RenderStyle.Contained;
+    public virtual int DefaultFontSize { get; set; } = Constants.DEFAULT_FONT_SIZE;
+    public virtual FontFamily DefaultFontFamily { get; set; } = Constants.DEFAULT_FONT_FAMILY;
+    public virtual ColorRgba BackgroundColor { get; set; } = ColorRgba.LimitlessBackground;
+    public virtual ColorRgba SecondaryBackgroundColor { get; set; } = ColorRgba.LimitlessPrimary;
+    public virtual ColorRgba ButtonBackgroundColor { get; set; } = ColorRgba.LimitlessAccent;
+    public virtual ColorRgba InputBackgroundColor { get; set; } = ColorRgba.LimitlessInput;
+    public virtual int DefaultCornerRadius { get; set; } = Constants.DEFAULT_ROUNDED_CORNER_RADIUS;
+    public virtual int DefaultLayoutItemSpacing { get; set; } = Constants.DEFAULT_ROUNDED_LAYOUT_ITEM_SPACING;
 }
