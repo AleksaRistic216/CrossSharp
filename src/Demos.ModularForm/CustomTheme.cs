634f5def95de337163a903220a14e6c635724d15
Aleksa Ristic <aristiccitsira@gmail.com>
2025-11-11 11:17:50 -0500

    Removed modular form demo

--- src/Demos.ModularForm/CustomTheme.cs
+++
@@ -1,9 +0,0 @@
-using CrossSharp.Themes;
-using CrossSharp.Utils.Enums;
-
-namespace Demos.ModularForm;
-
-public class CustomTheme : DefaultTheme
-{
-    public override RenderStyle Style { get; set; } = RenderStyle.Flat;
-}
